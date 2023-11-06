174b55d9add2b649e73aacece0c1e27c1839ceda
root <root@DESKTOP-04PNBN5>
2023-11-06 17:45:47 +0100

    Fix keyboard behavior

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,15 +1,13 @@
 import { FC, useCallback, useEffect, useState } from 'react';
-import Key from './Key/Key';
 import { ALLOWED_KEYS, NOTES, NOTE_TO_KEYS } from '../../utils/constants';
 import './Keyboard.scss';
-import useCallbackState from '../../utils/hooks/useCallbackState';
 
 interface KeyboardProps {
   offset?: number;
 }
 
 const Keyboard: FC<KeyboardProps> = () => {
-  const [pressedKeys, setPressedKeys] = useCallbackState<string[]>([]);
+  const [pressedKeys, setPressedKeys] = useState<string[]>([]);
   const [offset, setOffset] = useState<number>(0);
 
   const handleKeyDown = useCallback(
@@ -17,11 +15,7 @@ const Keyboard: FC<KeyboardProps> = () => {
       e.preventDefault();
       if (e.repeat) return;
       if (!pressedKeys.includes(e.key) && ALLOWED_KEYS.includes(e.key)) {
-        setPressedKeys(
-          (prevState) => [...prevState, e.key],
-          // TODO: dispatch
-          () => console.log('Down :', pressedKeys),
-        );
+        setPressedKeys([...pressedKeys, e.key]);
       }
     },
     [pressedKeys, setPressedKeys],
@@ -31,11 +25,7 @@ const Keyboard: FC<KeyboardProps> = () => {
     (e: KeyboardEvent) => {
       const index = pressedKeys.indexOf(e.key);
       if (index > -1) {
-        setPressedKeys(
-          (prevState) => prevState.filter((_, i) => i !== index),
-          // TODO: dispatch
-          () => console.log('Up :', pressedKeys),
-        );
+        setPressedKeys(pressedKeys.filter((_, i) => i !== index));
       }
     },
     [pressedKeys, setPressedKeys],
@@ -50,6 +40,10 @@ const Keyboard: FC<KeyboardProps> = () => {
       document.removeEventListener('keyup', handleKeyUp);
     };
   }, [handleKeyDown, handleKeyUp]);
+
+  useEffect(() => {
+    console.log(pressedKeys);
+  }, [pressedKeys]);
 
   return (
     <div className="keyboard-layout">
@@ -72,16 +66,50 @@ const Keyboard: FC<KeyboardProps> = () => {
           ({ note, freq }, index) =>
             index >= 24 + offset * 12 &&
             index < 48 + offset * 12 && (
-              <Key
-                key={index + Date.now()}
-                note={note}
-                freq={freq}
-                position={index}
-                isPressed={pressedKeys.includes(
+              <div
+                id={
                   NOTE_TO_KEYS[note as keyof typeof NOTE_TO_KEYS][
                     offset % 2 === 0 ? 0 : 1
-                  ],
-                )}
+                  ]
+                }
+                className={`key 
+                ${note.length === 3 ? 'flat' : ''}
+                ${
+                  pressedKeys.includes(
+                    NOTE_TO_KEYS[note as keyof typeof NOTE_TO_KEYS][
+                      offset % 2 === 0 ? 0 : 1
+                    ],
+                  )
+                    ? 'pressed'
+                    : ''
+                }
+                `}
+                onMouseDown={(e) => {
+                  console.log(e.currentTarget.id);
+                  if (
+                    !pressedKeys.includes(e.currentTarget.id) &&
+                    ALLOWED_KEYS.includes(e.currentTarget.id)
+                  ) {
+                    setPressedKeys([...pressedKeys, e.currentTarget.id]);
+                  }
+
+                  // TODO: dispatch
+                }}
+                onMouseUp={(e) => {
+                  console.log(e);
+                  const index = pressedKeys.indexOf(e.currentTarget.id);
+                  if (index > -1) {
+                    setPressedKeys(pressedKeys.filter((_, i) => i !== index));
+                  }
+                }}
+                onMouseOut={(e) => {
+                  console.log(e);
+                  const index = pressedKeys.indexOf(e.currentTarget.id);
+                  if (index > -1) {
+                    setPressedKeys(pressedKeys.filter((_, i) => i !== index));
+                  }
+                  // TODO: dispatch
+                }}
               />
             ),
         )}
